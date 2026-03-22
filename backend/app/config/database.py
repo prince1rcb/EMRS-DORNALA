094410af150ac6ed5c324dd1fef6ce3b30347f23
Prince bhardwaj <167130999+prince1rcb@users.noreply.github.com>
2026-03-22 06:36:35 +0530

    Update database.py

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
+MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://psbhardwaj181_db_user:MMnsjhQRFXAa3cFu@cluster0.sjrvasq.mongodb.net/?appName=Cluster0")
 DB_NAME = os.getenv("MONGODB_DB", "emrs_dornala")
 
 client = AsyncIOMotorClient(MONGODB_URI)
